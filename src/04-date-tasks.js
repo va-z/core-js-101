0d14ef4379a74d4e789aafb5c9018161f3447a57
va-z <vasilizadorojnyuk@gmail.com>
2020-12-01 16:43:38 +0500

    feat: add solutions to 04-date-tasks.js

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -1,6 +1,6 @@
 /* *******************************************************************************************
  *                                                                                           *
- * Plese read the following tutorial before implementing tasks:                              *
+ * Please read the following tutorial before implementing tasks:                              *
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Numbers_and_dates#Date_object
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date     *
  *                                                                                           *
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,13 +53,13 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  return new Date(date.setMonth(1, 29)).getMonth() === 1;
 }
 
 
 /**
- * Returns the string represention of the timespan between two dates.
+ * Returns the string representation of the timespan between two dates.
  * The format of output string is "HH:mm:ss.sss"
  *
  * @param {date} startDate
@@ -73,10 +73,17 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
-}
+function timeSpanToString(startDate, endDate) {
+  const format = (num, length) => (num < 10 ** (length - 1) ? String(num).padStart(length, '0') : num);
+  const format2 = (num) => format(num, 2);
 
+  const ms = Math.abs(startDate - endDate);
+  const ss = Math.floor(ms / 1000);
+  const mm = Math.floor(ss / 60);
+  const hh = Math.floor(mm / 60);
+
+  return `${format2(hh)}:${format2(mm % 60)}:${format2(ss % 60)}.${format(ms % 1000, 3)}`;
+}
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock
@@ -94,8 +101,13 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hh = date.getUTCHours() % 12;
+  const mm = date.getUTCMinutes();
+
+  const delta = Math.abs(0.5 * (60 * hh - 11 * mm));
+
+  return (delta <= 180 ? delta : 360 - delta) * (Math.PI / 180);
 }
 
 
